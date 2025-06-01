e9d2e77b50037464884a74e5aa0cd8e6457ab1c3
dishonoredd <suiratsasha06@gmail.com>
2025-06-01 15:03:59 +0300

    crud

--- post-controller.js
+++ post-controller.js
@@ -10,6 +10,52 @@ class PostController {
       res.status(500).json(err);
     }
   }
+  async getAll(req, res) {
+    try {
+      const posts = await Post.find({});
+      return res.json(posts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  async getOne(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "Id is not defined" });
+      }
+      const post = await Post.findById(id);
+      return res.json(post);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  async update(req, res) {
+    try {
+      const post = req.body;
+      if (!post._id) {
+        res.status(400).json({ message: "Id is not defined" });
+      }
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {
+        new: true,
+      });
+      return res.json(updatedPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "Id is not defined" });
+      }
+      const post = await Post.findByIdAndDelete(id);
+      res.json(post);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 }
 
 export default new PostController();
--- router.js
+++ router.js
@@ -3,10 +3,12 @@ import PostController from "./post-controller.js";
 
 const router = new Router();
 
-router.post("/posts", PostController.create);
-router.get("/posts", async () => {});
-router.get("/posts/:id", async () => {});
-router.put("/posts", async () => {});
-router.delete("/posts", async () => {});
+const url = "/posts";
+
+router.post(url, PostController.create);
+router.get(url, PostController.getAll);
+router.get(`${url}/:id`, PostController.getOne);
+router.put(url, PostController.update);
+router.delete(`${url}/:id`, PostController.delete);
 
 export default router;
